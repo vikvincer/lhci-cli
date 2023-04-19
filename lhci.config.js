0beedc60d0ed25078f37640003cf356e46fb9767
vikvincer <vikvincer@gmail.com>
2023-04-19 10:14:50 +0800

    first commit

---
+++ lhci.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // ...
+    server: {
+      // ...
+      storage: {
+        storageMethod: 'fs', // the file system storage method
+        basePath: './.lighthouseci', // where to store the files
+      },
+    },
+  };
+  
